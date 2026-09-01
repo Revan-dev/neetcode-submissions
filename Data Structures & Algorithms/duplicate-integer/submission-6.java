35c0bcdec929ec57ec63b2dd96ba53ccb8bfd789
Revan-dev <148819519+Revan-dev@users.noreply.github.com>
2026-09-01 13:24:16 -0400

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet hs = new HashSet<>();
+        Boolean bool = false;
+        for(int i = 0; i < nums.length; i++){
+
+            if( hs.contains(nums[i])){
+
+                bool = true;
+
+            }
+            
+            else{
+                hs.add(nums[i]);
+
+
+            }
+        }
+        return bool;
+
+
+        
+    }
+}
